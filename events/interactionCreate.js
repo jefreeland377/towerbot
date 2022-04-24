958319240cfd8e1ea065655cad8f73e569a8be7a
Jacob Freeland <jakefreeland13@gmail.com>
2022-04-24 16:53:00 -0400

    Deploy commands automatically on ready

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -8,14 +8,18 @@ module.exports =
 		const command = interaction.client.commands.get(interaction.commandName);
 
 		if (!command) return;
+		else
+		{
+			try
+			{
+				console.log(interaction.user.tag + " called /" + command.data.name + " in #" + interaction.channel.name + " with params " + JSON.stringify(command.data.options));
 
-		try
-		{
-			command.execute(interaction);
-		} catch (error)
-		{
-			console.error(error);
-			return interaction.reply({ content: 'Whoops, something weird just happened. Pinging <@86612976529838080> to take a look at it.', ephemeral: true });
+				command.execute(interaction);
+			} catch (error)
+			{
+				console.error(error);
+				return interaction.reply({ content: 'Whoops, something weird just happened. Pinging <@86612976529838080> to take a look at it.', ephemeral: true });
+			}
 		}
 	}
 }
--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('../config.json');
+
 module.exports =
 {
 	name: 'ready',
@@ -9,6 +15,21 @@ module.exports =
 		delay = false;
 		height = 0;
 		
-		console.log('Ready!\n');
+		//formerly deploy-commands.js, added into ready
+		const commands = [];
+		const commandFiles = fs.readdirSync(path.join(__dirname, "../commands")).filter(file => file.endsWith('.js'));
+
+		for (const file of commandFiles) {
+			const command = require(`../commands/${file}`);
+			commands.push(command.data.toJSON());
+		}
+
+		const rest = new REST({ version: '9' }).setToken(token);
+
+		rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+			.then(() => console.log('Successfully registered ' + commands.length + ' total commands.'))
+			.catch(console.error);
+
+		console.log('Ready!');
 	}
 }
